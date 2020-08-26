86b0fa3421e3ee5860ace9d4891cd503bbdee638
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 21:48:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,10 +27,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each do |person|
+  batch_badge_creator(attendees).each do |person|
     puts person
   end
-  assign_rooms.each do |assignment|
+  assign_rooms(attendees).each do |assignment|
     puts assignment
   end
 end
